30aaf3b531f48ea0969c373ba2bfe1edeb2299e7
nexdev <anwarnasir0970@gmail.com>
2024-08-19 20:24:17 +0300

    chore: show route for only the current riders route

--- lib/screens/order_acceptance_screen.dart
+++ lib/screens/order_acceptance_screen.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:slide_to_act/slide_to_act.dart';
 
 import '../services/order_service.dart';
 import 'customer_location.dart';
-import 'main_screen.dart';
 
 class OrderAcceptanceScreen extends StatefulWidget {
   final int countdownSeconds;
@@ -26,12 +27,14 @@ class _OrderAcceptanceScreenState extends State<OrderAcceptanceScreen> {
   late int _remainingSeconds;
   late Timer _timer;
   bool _orderPickedUp = false;
+  List<LatLng> _riderLocations = [];
 
   @override
   void initState() {
     super.initState();
     _remainingSeconds = widget.countdownSeconds;
     _startTimer();
+    _fetchRiderLocations();
   }
 
   void _startTimer() {
@@ -55,16 +58,30 @@ class _OrderAcceptanceScreenState extends State<OrderAcceptanceScreen> {
         content: const Text('Too late to pick the order.'),
         actions: [
           TextButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => const MainScreen()),
-              );
-            },
+            onPressed: () => Navigator.of(context).pop(),
             child: const Text('OK'),
           ),
         ],
       ),
     );
+  }
+
+  Future<void> _fetchRiderLocations() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final DatabaseReference ref = FirebaseDatabase.instance.ref('drivers');
+      final DataSnapshot snapshot = await ref.get();
+      final Map<dynamic, dynamic> data =
+          snapshot.value as Map<dynamic, dynamic>;
+
+      setState(() {
+        _riderLocations = data.values
+            .where((driver) => driver['email'] == user.email)
+            .map((driver) {
+          return LatLng(driver['latitude'], driver['longitude']);
+        }).toList();
+      });
+    }
   }
 
   @override
@@ -104,9 +121,7 @@ class _OrderAcceptanceScreenState extends State<OrderAcceptanceScreen> {
                         widget.order['location']['latitude'],
                         widget.order['location']['longitude'],
                       ),
-                      riderLocations: const [
-                        LatLng(7.6764406, 36.832514),
-                      ],
+                      riderLocations: _riderLocations,
                     ),
                   ),
                 );
